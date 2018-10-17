d4f982b92798210634f930088e03192644a156c4
Toto Corvidoni <TotoCorvidoni@gmail.com>
2018-10-17 16:58:32 -0300

    Add decoding feature

--- cipher.rb
+++ cipher.rb
@@ -4,7 +4,7 @@ require 'sinatra/reloader' if development?
 def cipher(string, shift)
   characters = string.chars # makes an array of the characters used
   # using % you limit the shift to under 26 which is the letter range from a-z, A-Z for examaple, both a shift of 1 and a shift of 27 would == 1
-  wrapper = ->(char, a_A) { (a_A + (x.ord - a_A + (shift % 26)) % 26).chr } # When it reaches z it wraps around and continues with a
+  wrapper = ->(char, a_A) { (a_A + (char.ord - a_A + (shift % 26)) % 26).chr } # When it reaches z it wraps around and continues with a
   encripted = []
   characters.each do |char| 
     if char =~ /[A-Z]/ # to encript only letters
@@ -16,9 +16,18 @@ def cipher(string, shift)
     end
     encripted << cripted
   end
-  encript.join
+  encripted.join
+end
+
+def convert(string, shift)
+  if params['method'] == 'encript'
+    cipher(string, shift)
+  elsif params['method'] == 'decript'
+    cipher(string, -shift)
+  end
 end
 
 get '/' do
-  erb :index 
+  message = convert(params['encript'], params['key'].to_i)
+  erb :index, :locals => { message: message }
 end
